55e0393c2fd17515ca46584cbcc2b98bb242b019
Salva <salvador.patty@ucb.edu.bo>
2024-03-05 10:05:26 -0400

    Segunda Prueba devuelve cadena con un 2

---
+++ src/primefactor.js
@@ -0,0 +1,12 @@
+class primeFactors
+  {
+      generate(n)
+      {
+        let primes=[]
+        if(n > 1){
+          primes.push(2);
+        }
+        return primes;
+      }
+  }
+export default primeFactors;
--- src/primefactor.spec.js
+++ src/primefactor.spec.js
@@ -1,16 +1,13 @@
+import primeFactors from "./primefactor.js";
+
 describe("Prime factors", () => {
     it("generate factors of 1", () => {
       let primefactors=new primeFactors();
       expect(primefactors.generate(1)).toEqual([]);
     });
-  
+    
+    it("generate factors of 2", () => {
+      let primefactors=new primeFactors();
+      expect(primefactors.generate(2)).toEqual([2]);
+    });
   });
-
-
-  class primeFactors
-  {
-      generate(n)
-      {
-        return [];
-      }
-  }
